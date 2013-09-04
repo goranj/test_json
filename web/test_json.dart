7d4d3e748d3226913ef5da10dee88cae8de3f80c
Göran Johansson <goran.johansson@longbay.se>
2013-09-04 10:00:15 +0200

    Next

--- web/test_json.dart
+++ web/test_json.dart
@@ -11,8 +11,8 @@ part 'random_student.dart';
 HttpRequest request;
 
 void main() {
-  print(new RandomStudent('5507172939', 'Göran Johansson', 'event'));
-//  loadData();
+//  print(new RandomStudent('5507172939', 'Göran Johansson', 'event'));
+  loadData();
 //  print(new Student('5507172939', 'Göran Johansson', '', '', ''));
 //  print('hej hopp!');
 }
